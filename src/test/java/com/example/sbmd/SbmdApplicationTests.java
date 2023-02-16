f421524a340a61a13e260b7311ea772082927494
Clean314 <pipie314@protonmail.com>
2023-02-16 21:07:03 +0900

    Initial commit

---
+++ src/test/java/com/example/sbmd/SbmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
